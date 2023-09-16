aa7c3a077a8c84a5efaacdf00aa3788835dd1e28
NickL <nick.lockington@outlook.com>
2023-09-17 00:18:31 +0100

    removed some stuff

--- register.py
+++ register.py
@@ -3,33 +3,6 @@ from tkinter import messagebox
 import sqlite3
 
 def registerUser():
-#function to validate login credentials from the database
-
-    # def check_login():
-    #     global loggedIn, entered_username
-    #     entered_username = username_entry.get()
-    #     entered_password = password_entry.get()
-
-    #     #connect to the database (or create it if it doesn't exist)
-    #     connection = sqlite3.connect("user_credentials.db")
-    #     cursor = connection.cursor()
-
-    #     #create the table if it doesn't exist
-    #     cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
-
-    #     #check if the user credentials are valid
-    #     cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (entered_username, entered_password))
-    #     user = cursor.fetchone()
-
-    #     #close the database connection
-    #     connection.close()
-
-    #     if user is not None:
-    #         messagebox.showinfo("Login Successful", "Welcome, " + entered_username + "!")
-    #         loggedIn = entered_username
-
-    #     else:
-    #         messagebox.showerror("Login Failed", "Invalid username or password.")
 
     #function to register a new user and store their information in the database
     def register_user():
@@ -68,22 +41,6 @@ def registerUser():
     #create the main window
     root = tk.Tk()
     root.title("Login and Registration Form")
-
-    # #username label and entry field
-    # username_label = tk.Label(root, text="Username:")
-    # username_label.pack()
-    # username_entry = tk.Entry(root)
-    # username_entry.pack()
-
-    # #password label and entry field
-    # password_label = tk.Label(root, text="Password:")
-    # password_label.pack()
-    # password_entry = tk.Entry(root, show="*")
-    # password_entry.pack()
-
-    # #login button
-    # login_button = tk.Button(root, text="Login", command=check_login)
-    # login_button.pack()
 
     #registration Section
     register_label = tk.Label(root, text="--- Register ---")
